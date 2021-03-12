7f8e5dd0a72b49390d70360b3930e2bed6213e8c
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-03-12 10:48:48 -0500

    Create a CodeGrade submission

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -7,7 +7,7 @@ export default class PlantList extends Component {
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
-
+  
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
